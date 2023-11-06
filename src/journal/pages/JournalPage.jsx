af250b88814679f4abf7cad6db0bd0dc35080b75
Nelson Gomez <n00gomez00@gmail.com>
2023-11-05 23:13:24 -0600

    adding new pages and design

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,14 @@
 
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+   
+    <Typography>Excepteur excepteur magna laborum ad amet occaecat cillum aute veniam qui nisi mollit aute. Reprehenderit dolor ipsum culpa occaecat ipsum. Dolore reprehenderit sunt anim ut nulla adipisicing velit laborum non ullamco eu. Pariatur consectetur cillum sint nostrud adipisicing tempor dolor proident cillum cupidatat veniam. Pariatur occaecat mollit tempor amet irure sit proident consequat dolor mollit consequat. Do nulla ullamco qui cupidatat eu.</Typography>
+
+    </JournalLayout>
   )
 }
